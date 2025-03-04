4cd93490c1b607984dbe9d09dddfa92a585cd130
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2025-03-04 06:35:23 -0500

    feat: getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -141,8 +141,11 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const millisecondsInDay = 1000 * 3600 * 24;
+  const day1 = Date.parse(dateStart);
+  const day2 = Date.parse(dateEnd);
+  return (day2 - day1) / millisecondsInDay + 1;
 }
 
 /**
