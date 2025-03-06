b7b1dcdeff3535e8a9bd0f918eb65bf9a97ff3c9
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2025-03-05 19:59:32 -0500

    feat: submit getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -308,8 +308,19 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth();
+  if (month < 4) {
+    return 1;
+  }
+  if (month > 3 && month < 7) {
+    return 2;
+  }
+  if (month > 6 && month < 9) {
+    return 3;
+  }
+
+  return 4;
 }
 
 /**
